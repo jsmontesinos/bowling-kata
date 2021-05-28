54837a8ae4dff64b6ff1e8c3e1d20a9a07428f9c
CosmeValera <cosmevalerareales@gmail.com>
2021-05-28 19:18:41 +0200

    destroy score method

--- java/src/main/java/BowlingGame.java
+++ java/src/main/java/BowlingGame.java
@@ -1,15 +1,20 @@
+
 class BowlingGame {
 
-  public int score(String frames) {
-    int number;
+    public int score(String frames) {
+        int number = 0;
 
-    try {
-      number = Integer.parseInt(frames.substring(0, 1));
-    } catch (NumberFormatException e) {
-      return 0;
+        try {
+            number = Integer.parseInt(frames.substring(0, 1));
+        } catch (NumberFormatException e) {
+            try {
+                number = Integer.parseInt(frames.substring(1, 2));
+            } catch (NumberFormatException e2) {
+
+            }
+        }
+
+        return number;
     }
-
-    return number;
-  }
 
 }
